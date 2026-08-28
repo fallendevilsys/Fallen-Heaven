2d87d5b48d4c0d44d7e4047bc6ac23cadf116bbc
FallenDevil <schanzi21@gmail.com>
2026-08-28 04:28:59 +0200

    Hide internal launcher from users

--- launch_app.ps1
+++
@@ -1,3 +0,0 @@
-$exe = Join-Path $PSScriptRoot "Fallen-Heaven Discord App.exe"
-if (-not (Test-Path -LiteralPath $exe)) { throw "App-EXE nicht gefunden: $exe" }
-Start-Process -FilePath $exe -WorkingDirectory $PSScriptRoot
